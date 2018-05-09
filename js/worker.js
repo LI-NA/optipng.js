8e3ba358c428f9a7f4d95d43cec3d47ea72fa5fe
Kwon Hyungjoo <hyung778@gmail.com>
2018-05-10 04:27:07 +0900

    File upload

---
+++ js/worker.js
@@ -0,0 +1,34 @@
+(function() {
+
+    // import optipng script.
+    importScripts('optipng.min.js');
+
+    // get stdout
+    function print(text) {
+        postMessage({ 'type': 'stdout', 'data': text });
+    }
+
+    /* bind on message event handler */
+    onmessage = function(event) {
+        var message = event.data;
+        if (message.type === "command") {
+            var args = message.arguments;
+
+            postMessage({
+                'type': 'start',
+                'data': JSON.stringify(args)
+            });
+
+            print('Received command: ' + JSON.stringify(args));
+
+            var time = performance.now();
+            var result = optipng(message.file.data, args, print);
+            var totalTime = performance.now() - time;
+
+            print('Finished processing (took ' + totalTime.toFixed(0) + 'ms)');
+
+            postMessage({ 'type': 'done', 'data': [result], 'time': totalTime });
+        }
+    };
+    postMessage({ 'type': 'ready' });
+})();
